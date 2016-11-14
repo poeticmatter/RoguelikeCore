b3ee8a1238909032db4f985b9f8a43fc735690e3
Itai <d00m.poet@gmail.com>
2016-11-14 21:24:52 +0200

    board game manager and position

---
+++ Assets/Game/BoardManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	private BoardPosition[,] boardPositions;
+	
+	void Awake()
+	{
+		SetupBoard(8,8);
+	}
+
+	
+	public void SetupBoard(int width, int height)
+	{
+		boardPositions = new BoardPosition[width, height];
+	}
+
+	public void UnregisterPosition(int x, int y)
+	{
+		if (x > boardPositions.GetLength(0) || 0 > x || y > boardPositions.GetLength(1) || 0 > y)
+		{
+			Debug.LogError("Trying to unregister out of board boaunds");
+			return;
+		}
+		if (boardPositions[x,y] ==null)
+		{
+			Debug.LogError("Attempting to unregister an empty location");
+		}
+
+		boardPositions[x, y] = null;
+	}
+
+	public void RegisterPosition(int x, int y, BoardPosition toRegister)
+	{
+		if (x > boardPositions.GetLength(0) || 0 > x || y > boardPositions.GetLength(1) || 0 > y)
+		{
+			Debug.LogError("Trying to register out of board boaunds");
+			return;
+		}
+
+		if (boardPositions[x, y] != null)
+		{
+			Debug.LogError("Overriding exisiting board postion " + boardPositions[x, y].name);
+		}
+
+		boardPositions[x, y] = toRegister;
+	}
+
+}
---
+++ Assets/Game/BoardPosition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardPosition : MonoBehaviour {
+
+	private int xPosition;
+	private int yPosition;
+
+	private BoardManager boardManager;
+
+	void Start ()
+	{
+		boardManager.RegisterPosition(xPosition, yPosition, this);
+	}
+
+	public void TeleportTo(int x, int y)
+	{
+		boardManager.UnregisterPosition(xPosition, yPosition);
+		xPosition = x;
+		yPosition = y;
+		boardManager.RegisterPosition(xPosition, yPosition, this);
+	}
+}
