ac9698a327ad18c53a71459cc06a2ff6a0f631a1
Itai <d00m.poet@gmail.com>
2016-11-15 22:37:04 +0200

    Move and melee actions with alternate hierarchy.

---
+++ Assets/Actions/MeleeAction.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Action))]
+public class MeleeAction : Action {
+
+	public int damage;
+
+	public int xDirection;
+	public int yDirection;
+
+	override public void Perform()
+	{
+		BoardPosition boardPosition = GetComponent<BoardPosition>();
+		BoardPosition adjacent = boardPosition.GetAdjacent(xDirection, yDirection);
+		if (adjacent == null)
+		{
+			Debug.LogError("Nothing to attack");
+			return;
+		}
+		Destructible target = adjacent.GetComponent<Destructible>();
+		if (target == null)
+		{
+			Debug.LogError("Target Indestructible");
+			return;
+		}
+		target.ApplyDamage(damage);
+
+	}
+
+	override public bool CanPerform()
+	{
+		BoardPosition boardPosition = GetComponent<BoardPosition>();
+		BoardPosition adjacent = boardPosition.GetAdjacent(xDirection, yDirection);
+		if (adjacent == null)
+		{
+			return false;
+		}
+		Destructible target = adjacent.GetComponent<Destructible>();
+		if (target == null)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	override public Action GetAlternate()
+	{
+		return GetComponent<Action>();
+	}
+}
---
+++ Assets/Actions/MoveAction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeleeAction))]
+public class MoveAction : Action {
+
+	public int xDirection;
+	public int yDirection;
+
+	override public void Perform()
+	{
+		BoardPosition boardPosition = GetComponent<BoardPosition>();
+		boardPosition.MoveDirection(xDirection, yDirection);
+	}
+
+	override public bool CanPerform()
+	{
+		return GetComponent<BoardPosition>().CanMoveDirection(xDirection, yDirection);
+	}
+
+	override public Action GetAlternate()
+	{
+		MeleeAction alternate = GetComponent<MeleeAction>();
+		alternate.xDirection = xDirection;
+		alternate.yDirection = yDirection;
+		return alternate;
+	}
+}
