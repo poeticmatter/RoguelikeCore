417f596a3318acb6daf85131746234788747d59b
Itai <d00m.poet@gmail.com>
2016-11-15 22:36:25 +0200

    Movement methods for BoardPosition

--- Assets/Game/BoardPosition.cs
+++ Assets/Game/BoardPosition.cs
@@ -6,18 +6,50 @@ public class BoardPosition : MonoBehaviour {
 	private int xPosition;
 	private int yPosition;
 
-	private BoardManager boardManager;
 
 	void Start ()
 	{
-		boardManager.RegisterPosition(xPosition, yPosition, this);
+		BoardManager.instance.RegisterPosition(xPosition, yPosition, this);
 	}
 
 	public void TeleportTo(int x, int y)
 	{
-		boardManager.UnregisterPosition(xPosition, yPosition);
+		BoardManager.instance.UnregisterPosition(xPosition, yPosition);
 		xPosition = x;
 		yPosition = y;
-		boardManager.RegisterPosition(xPosition, yPosition, this);
+		BoardManager.instance.RegisterPosition(xPosition, yPosition, this);
 	}
+
+	public void MoveDirection(int xDirection, int yDirection)
+	{
+		if (!CanMoveDirection(xDirection,yDirection))
+		{
+			Debug.LogError("Cannot ModeDirection");
+			return;
+		}
+		TeleportTo(xPosition + xDirection, yPosition + yDirection);
+	}
+
+	public bool CanMoveDirection (int xDirection, int yDirection)
+	{
+		if (Mathf.Abs(xDirection) > 1 || Mathf.Abs(yDirection) > 1)
+		{
+			Debug.LogError("Attempt to MoveDirection more than 1 space");
+			return false;
+		}
+		if (Mathf.Abs(xDirection) == Mathf.Abs(yDirection))
+		{
+			Debug.LogError("Attempt to MoveDirection diagonally or 0/0");
+			return false;
+		}
+		int xTo = xPosition + xDirection;
+		int yTo = yPosition + yDirection;
+        return BoardManager.instance.IsWithinBounds(xTo,yTo) && !BoardManager.instance.IsOccupied(xTo, yTo);	
+	}
+
+	public BoardPosition GetAdjacent(int xDirection, int yDirection)
+	{
+		return BoardManager.instance.GetOccupied(xPosition + xDirection, yPosition + yDirection);
+	}
+	
 }
