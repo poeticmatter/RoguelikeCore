0998be7e3d8f4a61eeacb0a131b2cf90ded0ba6c
Itai <d00m.poet@gmail.com>
2016-11-15 22:36:46 +0200

    Destructible component

---
+++ Assets/Components/Destructible.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+
+	public int startingHP;
+	private int currentHP;
+
+	void Awkake ()
+	{
+		currentHP = startingHP;
+	}
+
+	public void ApplyDamage(int damage)
+	{
+		currentHP -= damage;
+		if (currentHP <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}	
+}
