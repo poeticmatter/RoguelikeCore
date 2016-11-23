53d8fd30276993e08801585bc81b84ff84cd0caa
Itai <d00m.poet@gmail.com>
2016-11-23 21:52:18 +0200

    simple enemy script

---
+++ Assets/Actors/EnemyActor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyActor : Actor
+{
+
+	public override Action GetAction()
+	{
+		PlayerActor target = FindObjectOfType<PlayerActor>();
+		int xDirection = Mathf.Clamp(target.BoardPosition.xPosition - BoardPosition.xPosition, -1, 1);
+		if (xDirection!=0)
+		{
+			return GetMoveAction(xDirection, 0);
+		}
+		int yDirection = Mathf.Clamp(target.BoardPosition.yPosition - BoardPosition.yPosition, -1, 1);
+		if (yDirection != 0)
+		{
+			return GetMoveAction(0, yDirection);
+		}
+		return GetComponent<RestAction>();
+	}
+
+}
