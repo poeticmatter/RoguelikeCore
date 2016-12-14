51036cca150bc5515ed4e39fbdc13e5f4e1911c5
Itai <d00m.poet@gmail.com>
2016-12-14 23:55:10 +0200

    better operator and Equals for Position

--- Assets/Game/Position.cs
+++ Assets/Game/Position.cs
@@ -21,6 +21,14 @@ public class Position {
 
 	public override bool Equals(object obj)
 	{
+		if (ReferenceEquals(this, obj))
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
 		if (!(obj is Position))
 		{
 			return false;
@@ -36,17 +44,30 @@ public class Position {
 
 	public static bool operator == (Position p1, Position p2)
 	{
-		return p1._x == p2._x && p1._y == p2._y;
+		if (ReferenceEquals(p1, p2))
+		{
+			return true;
+		}
+		if (p1 == null || p2 == null)
+		{
+			return false;
+		}
+		return p1.Equals(p2);
 	}
 
 	public static bool operator != (Position p1, Position p2)
 	{
-		return p1._x != p2._x || p1._y != p2._y;
+		return !(p1 == p2);
 	}
 
 	public static Position operator + (Position p1, Position p2)
 	{
 		return new Position(p1._x + p2._x, p1._y + p2._y);
+	}
+
+	public static Position operator - (Position p1, Position p2)
+	{
+		return new Position(p1._x - p2._x, p1._y - p2._y);
 	}
 
 }
